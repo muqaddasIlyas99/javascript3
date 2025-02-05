c9a06c856b017adb89aec1aebb8c7952723f024b
muqaddasIlyas99 <ilyasmuqaddasilyas5@gmail.com>
2025-02-05 11:53:35 +0500

    update

---
+++ problem.js
@@ -0,0 +1,95 @@
+// Problems solve for non primitive
+
+// const product = {
+//     title : "Ball Pen",
+//     rate : 10,
+//     offer :4,
+//     price : 40,
+//  };
+//  console.log(product);
+//  cosole .log(product.title);
+//  console.log(product.rate);
+ // when we add something with string then it will be string. for example let x = 10 + "20"; console.log(x); output will be 1020. also "1234" + "5678" = "12345678".
+ 
+ // 2nd problem
+//  const profile = {
+//      userName: "_muqaddasilyas",
+//      isFollow: true,
+//      followers: 100,
+//      following: 200,
+//      posts: 100,
+//      detail: "I am a  junior  junior junior junior junior junior junior  web developer",
+//  };
+//  console.log( typeof profile);
+ 
+//  console.log(typeof  profile["userName"]);
+//  console.log(typeof  profile["isFollow"]);
+//  console.log(typeof  profile["followers"]);
+
+// Alert prompt confirm these are used for interact with users.
+// Alert is used to show a message to the user.
+// Prompt is used to get some data from the user.it will be a `questin to the user` such as "Enter your name".if user put some value then it will be stored in a variable.or if user press cancel then it will be stored in a variable as null.
+// Confirm is used to get a confirmation from the user.such as "Are you sure you want to delete this?".
+// alert("Enter the value of a");
+// let a = prompt("Enter the value of a");
+// document.write(a);
+// alert("you entered a value of a is " + (typeof a));
+// confirm
+// let b = confirm("Are you sure you want to delete this?");
+// if (b == true) {
+//     document.write("You pressed OK!");
+// }
+// else {
+//     document.write("You pressed Cancel!");
+// }
+
+// type conversion is two types 1 to js khd krleta h automatically and 1 wo kch functions deta h user apny mtabik krlyen means that we convert one data type to another data type. for example let x = 10; let y = "20"; let z = x + y; console.log(z); output will be 1020. because 10 is a number and 20 is a string. when we add a number with a string then it will be a string. for example let x = 10 + "20"; console.log(x); output will be 1020. also "1234" + "5678" = "12345678". 
+// let age = 10;
+// console.log (typeof age);
+// cosole.log (age);
+// plus operator is used for addition and concatenation. for example let x = 10 + 20; console.log(x); output will be 30. and let x = "10" + "20"; console.log(x); output will be 1020.it used for both number and string .but subtraction is only used in masths. but with two sting numbers by using multiplication it will be a number.  
+// let x = "100";
+// let y = null;
+// console.log = (x + y);
+// console.log = (typeof x); notanumber k kisi ko multiply krny sa wo number hojata h.bolean ko multiply krny sa wo number hojata h.three functions Boolean(), Number(), String().
+// let type = "5"-2;
+// console.log(type);
+// console.log(typeof type);
+// let type1 = "5"+2;
+// console.log(type1);
+// console.log(typeof type1);
+// let type2 = "5"*2;
+// console.log(type2);
+// console.log(typeof type2);
+// let type3 = "5"/2;
+// console.log(type3);
+// console.log(typeof type3);
+// let typr4 = "5" * "2";
+// console.log(typr4);
+// console.log(typeof typr4);
+// let type5 = "5" * "yes";
+// console.log(type5);
+// cosole.log(typeof type5);
+      // coverting number into string
+let price = 5;
+console.log(price);
+console.log(typeof price);
+let NewType = String(price);
+console.log(typeof NewType);
+    //   Boolean converting into string
+let isTrue = true;
+console.log(isTrue);
+console.log(typeof isTrue);
+let NewType1 =String(isTrue);
+console.log(NewType1);
+console.log(typeof NewType1);
+    //   string converting into number
+let age = "hello";
+console.log(age);
+console.log(typeof age);
+let NewType2 = Number(age);
+console.log(NewType2);
+console.log(typeof NewType2);
+let price1 = "100" ; 
+console.log(price1);
+console.log(typeof price1);
